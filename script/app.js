f4bca27fdcfdbef55e63d48d9b6141c2b665490c
Anya Schmidt <57494359+anyasc@users.noreply.github.com>
2020-03-28 16:09:08 -0300

    PWA stuff

--- script/app.js
+++ script/app.js
@@ -1,8 +1,20 @@
+// Service Worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('../sw.js')
+      .then(reg => console.log(`Service Worker registered ${reg}`))
+      .catch(err => console.log(`Error: ${err}`));
+  });
+}
+
+
+
+// Hamburguer
 const hamburguer = document.querySelector(".hamburguer");
 const sidebar = document.querySelector("#sidebar");
 const sideClose = document.querySelector(".side-close");
 
-console.log(sidebar);
 
 hamburguer.addEventListener('click', sidebarToggle)
 sideClose.addEventListener('click', sidebarToggle)
---
+++ sw.js
@@ -0,0 +1,47 @@
+const cacheName = 'v1';
+
+const cacheAssets = [
+  'index.html'
+]
+
+// Install
+
+self.addEventListener('install', e => {
+  console.log('Service Worker installed')
+})
+
+// Activate
+
+self.addEventListener('activate', e => {
+  console.log('Service Worker activated')
+  e.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== cacheName) {
+            console.log('Service Worker: cleaning old cache');
+            return caches.delete(cache);
+          }
+        })
+      )
+    })
+  );
+})
+
+
+// Fetch
+self.addEventListener('fetch', e => {
+  console.log('Service Worker fetching');
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const resClone = res.clone();
+        caches
+          .open(cacheName)
+          .then(cache => {
+            cache.put(e.request, resClone);
+          });
+        return res
+      }).catch(err => caches.match(e.request).then(res => res))
+  );
+});
