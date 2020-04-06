e6f585a6332de39118663a14675f37d594835a7c
Anya Schmidt <57494359+anyasc@users.noreply.github.com>
2020-04-06 10:36:52 -0300

    dias

--- script/app.js
+++ script/app.js
@@ -30,3 +30,28 @@ function sidebarToggle() {
   sidebar.classList.toggle('active');
   hamburguer.classList.toggle('active');
 }
+
+
+// API
+const tempMax = document.querySelector("#tempMax");
+const tempMin = document.querySelector("#tempMin");
+const semana = ["Domingo", "Segunda-feira", "Terça-Feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"];
+const semanaAbr = ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sab"];
+const mes = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
+let today = new Date();
+document.querySelector("#hoje").innerText = semana[today.getDay()]
+document.querySelector("#diacompleto").innerText = semana[today.getDay()] + ", " + today.getDate() + " de " + mes[today.getMonth()].toLowerCase() + " de " + today.getFullYear();
+document.querySelector("#amanha").innerText = semanaAbr[today.getDay()+1]
+document.querySelector("#da").innerText = semanaAbr[today.getDay()+2]
+document.querySelector("#dda").innerText = semanaAbr[today.getDay()+3]
+document.querySelector("#ddda").innerText = semanaAbr[today.getDay()+4]
+
+const darksky = "https://api.darksky.net/forecast/9d783ced70007d510216381c3579ba6a/-23.5489,-46.6388?lang=pt&units=si&exclude=minutely,hourly,flags"
+
+function updateAPI() {
+  fetch(darksky)
+    .then(function(data) {
+      tempMax.innerText = data.daily[0].temperatureHigh;
+      tempMin.innerText = data.daily[0].temperatureLow;
+    })
+}
