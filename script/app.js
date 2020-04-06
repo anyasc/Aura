02f5e61b545ff6e1b368d11476d6ec395708ff0f
Anya Schmidt <57494359+anyasc@users.noreply.github.com>
2020-04-06 19:38:04 -0300

    localforage

--- script/app.js
+++ script/app.js
@@ -4,16 +4,60 @@ const tempMin = document.querySelector("#tempMin");
 const semana = ["Domingo", "Segunda-feira", "Terça-Feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"];
 const semanaAbr = ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sab"];
 const mes = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
+let localConfig = {
+  tempAlta: document.querySelector("#tempAltaCheck"),
+  tempBaixa: document.querySelector("#tempBaixaCheck"),
+  seco: document.querySelector("#secoCheck"),
+  umido: document.querySelector("#umidoCheck"),
+  beberAgua: document.querySelector("#aguaCheck"),
+  pulseira: document.querySelector("#pulseiraCheck")
+}
+console.log(localConfig)
 
-window.addEventListener("load", function() {
+window.addEventListener("load", function () {
   let today = new Date();
   document.querySelector("#hoje").innerText = semana[today.getDay()]
   document.querySelector("#diacompleto").innerText = semana[today.getDay()] + ", " + today.getDate() + " de " + mes[today.getMonth()].toLowerCase() + " de " + today.getFullYear();
-  document.querySelector("#amanha").innerText = semanaAbr[today.getDay()+1]
-  document.querySelector("#da").innerText = semanaAbr[today.getDay()+2]
-  document.querySelector("#dda").innerText = semanaAbr[today.getDay()+3]
-  document.querySelector("#ddda").innerText = semanaAbr[today.getDay()+4]
+  document.querySelector("#amanha").innerText = semanaAbr[today.getDay() + 1]
+  document.querySelector("#da").innerText = semanaAbr[today.getDay() + 2]
+  document.querySelector("#dda").innerText = semanaAbr[today.getDay() + 3]
+  document.querySelector("#ddda").innerText = semanaAbr[today.getDay() + 4]
   updateAPI();
+
+  localforage.getItem("config")
+    .then(value => {
+      if (value === null) {
+        console.log("Configurações não encontradas");
+        localforage.setItem("config", {
+          tempAlta: 1,
+          tempBaixa: 1,
+          seco: 1,
+          umido: 1,
+          beberAgua: 0,
+          pulseira: 1
+        })
+          .then(value => {
+            Object.keys(value).forEach(item => {
+              console.log(item, value[item]);
+              if (value[item] === 0) {
+                uncheck(localConfig[item])
+              } else {
+                check(localConfig[item])
+              }
+            })
+          })
+      } else {
+        Object.keys(value).forEach(item => {
+          console.log(item, value[item]);
+          if (value[item] === 0) {
+            uncheck(localConfig[item])
+          } else {
+            check(localConfig[item])
+          }
+        })
+      }
+    });
+
 });
 
 
@@ -49,22 +93,23 @@ const proxy = "https://cors-anywhere.herokuapp.com/"
 const previsao = document.querySelector("#previsao");
 
 function updateAPI() {
-  fetch(proxy+darksky)
+  fetch(proxy + darksky)
     .then(res => {
-      return res.json()}
-      )
+      return res.json()
+    }
+    )
     .then(data => {
       document.querySelector("#tempMax").innerText = Math.round(data.daily.data[0].temperatureHigh);
       document.querySelector("#tempMin").innerText = Math.round(data.daily.data[0].temperatureLow);
-      document.querySelector("#chuva").innerText = data.daily.data[0].precipProbability*100;
+      document.querySelector("#chuva").innerText = data.daily.data[0].precipProbability * 100;
 
       document.querySelector("#compTempMax").innerText = Math.round(data.daily.data[0].temperatureHigh);
       document.querySelector("#compTempMin").innerText = Math.round(data.daily.data[0].temperatureLow);
-      document.querySelector("#compChuva").innerText = data.daily.data[0].precipProbability*100;
+      document.querySelector("#compChuva").innerText = data.daily.data[0].precipProbability * 100;
 
       document.querySelector("#amanhaTempMax").innerText = Math.round(data.daily.data[1].temperatureHigh);
       document.querySelector("#amanhaTempMin").innerText = Math.round(data.daily.data[1].temperatureLow);
-      
+
       document.querySelector("#daTempMax").innerText = Math.round(data.daily.data[1].temperatureHigh);
       document.querySelector("#daTempMin").innerText = Math.round(data.daily.data[1].temperatureLow);
 
@@ -74,4 +119,32 @@ function updateAPI() {
       document.querySelector("#dddaTempMax").innerText = Math.round(data.daily.data[1].temperatureHigh);
       document.querySelector("#dddaTempMin").innerText = Math.round(data.daily.data[1].temperatureLow);
     })
+}
+
+
+// Config
+
+function check(box) {
+  box.checked = true;
+}
+
+function uncheck(box) {
+  box.checked = false
+}
+
+Object.keys(localConfig).forEach(item => {
+  localConfig[item].addEventListener("click", updateConfig)
+})
+
+function updateConfig() {
+  let newConfig = {};
+  Object.keys(localConfig).forEach(item => {
+    if (localConfig[item].checked === true) {
+      newConfig[item] = 1
+    } else {
+      newConfig[item] = 0
+    }
+  })
+  localforage.setItem("config", newConfig)
+    .then(value => console.log(value))
 }
